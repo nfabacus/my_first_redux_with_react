75a980b4cd3d234b65e91dde5e5902f5b7ae6d7b
Noby Fujioka <n.fujioka@abacusmaths.info>
2016-05-18 05:56:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_first_redux_with_react
+This is my first attempt to use redux for React.
+I am following along the tutorial by Stephen Grider and using his Redux starter code 'ReduxSimpleStarter'.
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+		{ title: 'Javascript: The Good Parts'},
+		{ title: 'The Secret Life of Nora Mendosa'},
+		{ title: 'Hairy Potter: Prisoner of Lala Land'},
+		{ title: 'Eloquent Ruby'}
+	]
+}
